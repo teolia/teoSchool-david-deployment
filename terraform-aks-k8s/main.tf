6b95a15121cd9c4874382ecd04a09157695804d5
daespinel <dafespinelsa@unal.edu.co>
2021-06-11 15:53:46 +0000

    updated files

--- terraform-aks-k8s/main.tf
+++ terraform-aks-k8s/main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  version = "~>2.0"
   features {}
 }
 
